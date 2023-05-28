7711be3bc0ed7d2885ff87c97fc1291b4fcceffd
JosuePimentel <josue.farias.pimentel@gmail.com>
2023-05-28 12:21:19 -0300

    JS

--- JS/index.js
+++ JS/index.js
@@ -62,3 +62,88 @@ let r
 console.log(r, typeof(r)) // Por predefinição, qq variavel criada sem nenhum conteudo dentro, por definifição recebe undefined e o tipo undefined.
 const t = 'As constantes devem receber o conteudo na criação dela.'
 console.log(t)
+
+console.log("")
+console.log("Entradas") // Biblioteca usada npm --install readline-sync
+const input = require("readline-sync")
+const n2 = input.question('Informe um numero\n')
+console.log(n2, typeof(n2))
+console.log(Number(n2), typeof(Number(n2)))
+
+let n3
+do{
+    n3 = Number(input.question("Qual a senha?\n"))
+    if(n3 == 13) {
+        console.log("Parabens, vc acertou a senha!")
+    } else {
+        console.log("Digite a senha novamente.")
+    }
+} while(n3 !== 13) 
+
+console.log("")
+
+console.log("Arrays")
+let arr = ['Josué', 19, 1.65, 'Rio Verde/GO']
+
+console.log("For of")
+for (const elemento of arr) {
+    console.log(elemento, arr)
+}
+console.log()
+
+console.log("ForEach")
+arr.forEach((ele,index,array) => {
+    console.log(ele, index, array)
+})
+console.log()
+
+console.log("For in")
+for (const index in arr) {
+    console.log(index, arr[index], arr)
+}
+
+console.log()
+console.log("Metodos Arrays")
+console.log("slice")
+let fatia = arr.slice(0,2) // (0,2[
+console.log(fatia)
+console.log(arr.slice(2))
+
+console.log()
+console.log("push/unshift/pop/shift")
+let arr1 = new Array
+arr1.push(10, 20, 30) //push -> Elementos sao jogados para a ultima posição
+arr1.push(40)
+arr1.unshift(0) // unshift -> Elemento é jogado para o início do array
+console.log(arr1)
+
+let arr2 = new Array
+arr1.pop() // pop -> Remove o ultimo elemento do array
+arr2.pop()
+console.log(arr1, arr2)
+
+const elementoRemovido = arr1.shift() // Remove o primeiro elemento do array || Retorna o elemento removido
+arr2.shift()
+console.log(arr1, arr2, elementoRemovido)
+
+console.log()
+console.log("Concat")
+console.log(arr.concat(arr1))
+console.log(arr1.concat(arr))
+
+
+let arr3 = [3,4,5,3,5,1,1,3]
+console.log()
+console.log("lastIndexOf/IndexOf")
+console.log(arr3.indexOf(3)) // lê o array da esquerda pra direita e retorna a primeira posição do valor encontrado
+console.log(arr3.lastIndexOf(3)) // lê o array da direita pra esquerda e retorna a primeira posição do valor encontrado
+console.log(arr3.indexOf(10)) // Caso o valor não seja encontrada, ele retorna -1
+
+console.log()
+console.log("Include")
+console.log(arr3.includes(3))
+console.log(arr3.includes(10)) 
+
+console.log()
+console.log("Reverse") // Inverte o array
+console.log(arr3.reverse())
